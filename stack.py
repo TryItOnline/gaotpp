9814e050c3c1249d007b73f33f595e42f4ac4eed
EasterlyIrk <riker.wachtler@gmail.com>
2016-07-23 14:02:15 -0700

    Update stack.py

--- stack.py
+++ stack.py
@@ -13,24 +13,10 @@ class Stack(list):
             return 0
         return list.pop(self)
 
-    def get(self):
-        return self
-
-    def clear(self):
-        self = Stack([])
-        self.temp = None
-
     def peek(self):
         if len(self) == 0:
             return 0
         return self[-1]
 
-    def weave(self, other):
-        if len(self) == len(other):
-            weaved = sum(zip(self, other), [])
-        else:
-            stacks = sorted([self, other])
-            extra = reduce(lambda a,b:a-b, map(len, stacks))
-            weaved = sum(zip(*stacks), [])
-            weaved += stacks[1][-abs(extra):]
-        return Stack(weaved)
+    def get(self):
+        return self
